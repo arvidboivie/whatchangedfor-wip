aabaa1d6bd956576241c3a12ab54db479dbf97c1
Arvid Boivie <arvid.boivie@gmail.com>
2024-06-26 21:40:20 +0200

    Add logic to datafeed

--- libs/parser/data-access/datafeed-api/src/lib/datafeed-api.spec.ts
+++
@@ -1,7 +0,0 @@
-import { datafeedApi } from './datafeed-api';
-
-describe('datafeedApi', () => {
-  it('should work', () => {
-    expect(datafeedApi()).toEqual('datafeed-api');
-  });
-});
--- libs/parser/data-access/datafeed-api/src/lib/datafeed-api.ts
+++ libs/parser/data-access/datafeed-api/src/lib/datafeed-api.ts
@@ -1,3 +1,34 @@
-export function datafeedApi(): string {
-  return 'datafeed-api';
+export class Datafeed {
+  private readonly BASE_URL = `https://www.dota2.com/datafeed`;
+
+  private readonly datastore = new Map<string, any>();
+
+  public async patches(): Promise<any> {
+    return this.getData('patchnoteslist?language=english');
+  }
+
+  public async heroes(): Promise<any> {
+    return this.getData('herolist?language=english');
+  }
+
+  public async items(): Promise<any> {
+    return this.getData('itemlist?language=english');
+  }
+
+  public async abilities(): Promise<any> {
+    return this.getData('abilitylist?language=english');
+  }
+
+  private async getData(resource: string): Promise<any> {
+    if (this.datastore.has(resource)) {
+      return this.datastore.get(resource);
+    }
+    this.datastore.set(resource, await this.makeFetchHappen(resource));
+    return this.datastore.get(resource);
+  }
+
+  private async makeFetchHappen(resource: string): Promise<any> {
+    const response = await fetch(`${this.BASE_URL}/${resource}`);
+    return await response.json();
+  }
 }
